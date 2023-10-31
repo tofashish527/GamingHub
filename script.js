c4dd7ab40effb2e88e561adf857d62f560003668
tawsifbhuiyan <109042623+tawsifbhuiyan@users.noreply.github.com>
2023-11-01 02:12:59 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,175 @@
+
+// @media (max-width: 768px) {
+//     /* Styles for screens up to 768px wide */
+// }
+
+// @media (max-width: 480px) {
+//     /* Styles for screens up to 480px wide */
+// }
+
+// @media (max-width: 768px) {
+//     .home-content h1 {
+//         font-size: 2rem; /* Adjust the font size for smaller screens */
+//     }
+// }
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadButton = document.getElementById('download-button');
+
+    downloadButton.addEventListener('click', function() {
+        window.open('fifa23.html', '_blank');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadButton = document.getElementById('download-button1');
+
+    downloadButton.addEventListener('click', function() {
+        window.open('gta5.html', '_blank');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadButton = document.getElementById('download-button2');
+
+    downloadButton.addEventListener('click', function() {
+        window.open('mafia.html', '_blank');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadButton = document.getElementById('download-button3');
+
+    downloadButton.addEventListener('click', function() {
+        window.open('crazytaxi.html', '_blank');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadButton = document.getElementById('download-button4');
+
+    downloadButton.addEventListener('click', function() {
+        window.open('houseofthedead.html', '_blank');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadButton = document.getElementById('download-button5');
+
+    downloadButton.addEventListener('click', function() {
+        window.open('donbradman.html', '_blank');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadButton = document.getElementById('download-button6');
+
+    downloadButton.addEventListener('click', function() {
+        window.open('seaofstars.html', '_blank');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadButton = document.getElementById('download-button7');
+
+    downloadButton.addEventListener('click', function() {
+        window.open('streetfighter6.html', '_blank');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadButton = document.getElementById('download-button8');
+
+    downloadButton.addEventListener('click', function() {
+        window.open('sanandreas.html', '_blank');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadButton = document.getElementById('download-button9');
+
+    downloadButton.addEventListener('click', function() {
+        window.open('eurotrucksimulator.html', '_blank');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadButton = document.getElementById('download-button10');
+
+    downloadButton.addEventListener('click', function() {
+        window.open('pubg.html', '_blank');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadButton = document.getElementById('download-button11');
+
+    downloadButton.addEventListener('click', function() {
+        window.open('cyberpunk.html', '_blank');
+    });
+});
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("btn-1").addEventListener("click", function() {
+    
+    var downloadLink = "https://www.pastemytxt.com/ZLfczASpfa";
+
+
+    window.location.href = downloadLink;
+});
+
+document.getElementById("btn-2").addEventListener("click", function() {
+    
+    var downloadLink = "https://www.pastemytxt.com/rypvRrJ6zK";
+
+    
+    window.location.href = downloadLink;
+});
+
+
+
+document.getElementById("btn-3").addEventListener("click", function() {
+    
+    var downloadLink = "https://www.instant-gaming.com/en/186-buy-rockstar-grand-theft-auto-v-pc-game-rockstar/?igr=softonic1";
+
+    
+    window.location.href = downloadLink;
+});
+
+document.getElementById("btn-4").addEventListener("click", function() {
+    
+    var downloadLink = "https://www.instant-gaming.com/en/186-buy-rockstar-grand-theft-auto-v-pc-game-rockstar/?igr=softonic1";
+
+    
+    window.location.href = downloadLink;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
